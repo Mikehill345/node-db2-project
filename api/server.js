befbc3ef1ba24b0e4955d57742b4aeebaf282d35
mike hill <michael-hill@lambdastudents.com>
2020-11-03 15:37:40 -0500

    finished mvp

---
+++ api/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+require('colors')
+const express = require('express');
+const helmet = require('helmet');
+
+const carRouter = require('../cars/cars-router')
+const db = require('../data/connection.js')
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use('/api/cars', carRouter);
+
+server.get('/', (req, res) => {
+    res.json('Server is up')
+})
+
+
+module.exports = server;
